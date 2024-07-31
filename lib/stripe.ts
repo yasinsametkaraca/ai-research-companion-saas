4ffc67fc25bc440498f981201db775ee0ce2ac10
Yasin Samet Karaca <ysk@outlook.com.tr>
2024-07-31 15:56:03 +0300

    refactor: update stripe version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,6 +2,6 @@ import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
     typescript: true,
-    apiVersion: "2022-11-15",
+    apiVersion: "2024-04-10",
 });
 
